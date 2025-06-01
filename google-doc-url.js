294521d7ac66ab691e97d92e1c9f6e6b2518f162
Shai Cohen <scohen64@gmail.com>
2025-06-01 14:35:15 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nUF9PejX7gwU0EcS1JW0vZLJRrwplmW1D0X9yal3pNI/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nh8bW0zIFhXJxhWsmfdW_gQ9uBy7frrV3k5T-inJiiA/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
